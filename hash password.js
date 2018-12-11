11419e41cac99e39764ee340f72e5d878aeca0dc
tsailawrence <45750993+tsailawrence@users.noreply.github.com>
2018-12-11 18:58:03 +0800

    Add files via upload

---
+++ hash password.js
@@ -0,0 +1,24 @@
+var bcrypt = require('bcrypt.js');
+
+function createNewUser(name, password) {
+    var hash = bcrypt.hashSync(password);
+    var userObj = { name: name, password: hash };
+
+    UserModel.create(userObj, function (err, result) {
+        if (err)
+            return handleError(err);
+
+    });
+}
+
+
+function validateUser(name, password) {
+    var hash = bcrypt.hashSync(password);
+    UserModel.findone({ name: name }, function (err, user) {
+        if (err)
+            return handleError(err);
+
+
+        return bcrypt.compareSync(hash, user.password);
+    })
+}
